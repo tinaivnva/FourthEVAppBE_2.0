31d770f8b3754a8e5e8a35ec35452ed27bee00cb
tsvetymanolova <ts.manolova23@gmail.com>
2023-07-21 11:58:24 +0300

    updated CarModels

--- TravelEasy.EV.API/Models/CarModels/CarRequestModel.cs
+++ TravelEasy.EV.API/Models/CarModels/CarRequestModel.cs
@@ -2,5 +2,6 @@
 {
     public class CarRequestModel
     {
+        public int Id { get; set; }
     }
 }
---
+++ TravelEasy.EV.API/Models/CarModels/CarResponceModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelEasy.EV.API.Models.CarModels
+{
+    public class CarResponceModel
+    {
+        public string? Brand { get; set; }
+
+        public string? Model { get; set; }
+
+        public int HorsePowers { get; set; }
+
+        public int Range { get; set; }
+
+        public decimal PricePerDay { get; set; }
+    }
+}
