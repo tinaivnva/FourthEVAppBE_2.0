465c082363bd98175bfcb32b6a4b4eb47b744183
tsvetymanolova <ts.manolova23@gmail.com>
2023-07-26 17:31:43 +0300

    FIX BOOKING

--- Service/Booking/BookingService.cs
+++ Service/Booking/BookingService.cs
@@ -1,13 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TravelEasy.EV.DataLayer;
+using TravelEasy.ElectricVehicles.DB.Models;
+using Service.Cars.Interfaces;
 
 namespace Service.Booking
 {
     public class BookingService : IBookingService
     {
-        
+        private readonly ICarsService _carsService;
+        private readonly IBookingService _bookingService;
+        private readonly ElectricVehiclesContext _EVContext;
+
+        public BookingService(ElectricVehiclesContext EVContext, IBookingService bookingService, ICarsService carsService) 
+        {
+            _EVContext = EVContext;
+            _carsService = carsService;
+        }
+        /*public void AddBooking(Booking booking)
+        {
+            _EVContext.Booking.Add(booking);
+            _EVContext.SaveChanges();
+        }*/
+
+        public bool CheckIfBookingExists(int bookingId)
+        {
+            return _EVContext.Booking.Where(b => b.Id == bookingId).Any();
+        }
+
+        public bool CheckIfBookingExists()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ElectricVehicle> GetAvailableVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        /*public ICollection<ElectricVehicle> GetAvailableVehicles()
+        {
+            ICollection<ElectricVehicle> availiableVehicles = new List<ElectricVehicle>();
+            var vehicles = _carsService
+            throw new NotImplementedException();
+        }*/
+
+        public ICollection<ElectricVehicle> GetBookedVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBookingByCarID(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetBookingByID(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Service/Booking/IBookingService.cs
+++ Service/Booking/IBookingService.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelEasy.EV.DataLayer;
 using TravelEasy.ElectricVehicles.DB.Models;
 
 namespace Service.Booking
 {
     public interface IBookingService
-    { 
+    {
+        //public void AddBooking(Booking booking);
+        public bool CheckIfBookingExists();
+        public void GetBookingByID(int bookingId);
+        public void GetBookingByCarID(int bookingId);
+        public ICollection<ElectricVehicle> GetBookedVehicles();
+        public ICollection<ElectricVehicle> GetAvailableVehicles();
+
     }
 }
